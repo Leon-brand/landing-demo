564fbd04ef1b44ae5dc2249316f0835a967767a1
Leon <gotama8484@gmail.com>
2025-11-19 22:28:51 -0600

    Quito AppProvider del main

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
- /*  base: 'https//Leon-brand.github.io/landing-demo', */
+ /*  base: 'https://Leon-brand.github.io/landing-demo', */
  base: "/landing-demo/"
 })
